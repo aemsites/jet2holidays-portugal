de56229a8c691f888c85cb24906ac6d6da9a238b
anica <anica@adobe.com>
2024-10-08 16:50:36 +0300

    initial version for simple cards with image and overlay

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -6,10 +6,21 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
+    const anchor = document.createElement('a');
+    anchor.href = '#';
+    const figure = document.createElement('figure');
+    figure.className = 'cards-card-figure';
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'figure-image';
+        figure.append(div);
+      } else if (div.children.length === 1 && div.querySelector('p')) {
+        div.className = 'figure-image-overlay';
+        figure.append(div);
+      } else div.className = 'cards-card-body';
     });
+    anchor.append(figure);
+    li.append(anchor);
     ul.append(li);
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
