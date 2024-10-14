492b741bcb80d2f3532667481ad0aefa08d60a7f
anica <anica@adobe.com>
2024-10-14 15:33:02 +0200

    added icon variation

--- blocks/simple-cards/simple-cards.js
+++ blocks/simple-cards/simple-cards.js
@@ -12,16 +12,33 @@ export default function decorate(block) {
     const figure = document.createElement('figure');
     figure.className = 'simple-cards-card-figure';
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) {
+      if (div.children.length === 1 && div.querySelector('picture')) { // simple cards image
         const image = div.querySelector('picture');
         image.className = 'figure-image';
         figure.append(image);
-      } else if (div.children.length === 1 && div.querySelector('p')) {
+      } else if (div.children.length === 1 && div.querySelector('p')) { // simple cards overlay with no icons
+        ul.className = 'simple-cards-grid-regions';
         const figCaption = document.createElement('figcaption');
         figCaption.textContent = div.querySelector('p').querySelector('strong').querySelector('a').title;
         figCaption.className = 'figure-image-overlay';
         anchor.href = div.querySelector('p').querySelector('strong').querySelector('a').href;
         figure.append(figCaption);
+      } else if (div.querySelectorAll('p').length === 2) { // simple cards overlay cards with icons
+        ul.className = 'simple-cards-grid-activities';
+        const figCaptionWithIcon = document.createElement('figcaption');
+        const figCaptionSpan = document.createElement('span');
+        const activityDiv = document.createElement('div');
+        const iconImage = div.querySelectorAll('p')[1].querySelector('strong').querySelector('span').querySelector('img');
+        iconImage.className = 'figure-span-overlay-icon';
+        activityDiv.textContent = div.querySelectorAll('p')[0].querySelector('strong').querySelector('a').title;
+        activityDiv.className = 'figure-span-overlay-text';
+        figCaptionSpan.className = 'figure-span-overlay';
+        figCaptionSpan.append(iconImage);
+        figCaptionSpan.append(activityDiv);
+        figCaptionWithIcon.append(figCaptionSpan);
+        figCaptionWithIcon.className = 'figure-image-overlay';
+        anchor.href = div.querySelectorAll('p')[0].querySelector('strong').querySelector('a').href;
+        figure.append(figCaptionWithIcon);
       }
       div.remove();
     });
