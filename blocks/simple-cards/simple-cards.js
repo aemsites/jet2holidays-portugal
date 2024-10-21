b44606d0062e5337ff6ac81aa0f7f7c5ef3ac6a6
anica <anica@adobe.com>
2024-10-21 11:19:14 +0200

    updated js and css

--- blocks/simple-cards/simple-cards.js
+++ blocks/simple-cards/simple-cards.js
@@ -5,44 +5,55 @@ export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
     const anchor = document.createElement('a');
+    const figure = document.createElement('figure');
+
     anchor.className = 'simple-cards-card-anchor';
     anchor.href = '#';
-    const figure = document.createElement('figure');
     figure.className = 'simple-cards-card-figure';
+
+    while (row.firstElementChild) li.append(row.firstElementChild);
+
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) { // simple cards image
+      /* simple cards image */
+      if (div.children.length === 1 && div.querySelector('picture')) {
         const image = div.querySelector('picture');
-        // image.width = '600';
-        // image.height = '400';
         image.className = 'figure-image';
         figure.append(image);
-      } else if (div.children.length === 1 && div.querySelector('p')) { // simple cards overlay with no icons
-        ul.className = 'simple-cards-grid-regions';
+        div.remove();
+        return;
+      }
+
+      /* simple cards overlay with no icons */
+      if (div.children.length === 1 && div.querySelector('p')) {
         const figCaption = document.createElement('figcaption');
+        anchor.href = div.querySelector('p').querySelector('strong').querySelector('a').href;
         figCaption.textContent = div.querySelector('p').querySelector('strong').querySelector('a').title;
         figCaption.className = 'figure-image-overlay';
-        anchor.href = div.querySelector('p').querySelector('strong').querySelector('a').href;
+        ul.className = 'simple-cards-grid-regions';
         figure.append(figCaption);
-      } else if (div.querySelectorAll('p').length === 2) { // simple cards overlay cards with icons
-        ul.className = 'simple-cards-grid-activities';
+        div.remove();
+        return;
+      }
+
+      /* simple cards overlay cards with icons */
+      if (div.querySelectorAll('p').length === 2) {
         const figCaptionWithIcon = document.createElement('figcaption');
         const figCaptionSpan = document.createElement('span');
         const activityDiv = document.createElement('div');
         const iconImage = div.querySelectorAll('p')[1].querySelector('strong').querySelector('span').querySelector('img');
+        anchor.href = div.querySelectorAll('p')[0].querySelector('strong').querySelector('a').href;
         iconImage.className = 'figure-span-overlay-icon';
+        ul.className = 'simple-cards-grid-activities';
         activityDiv.textContent = div.querySelectorAll('p')[0].querySelector('strong').querySelector('a').title;
         activityDiv.className = 'figure-span-overlay-text';
         figCaptionSpan.className = 'figure-span-overlay';
         figCaptionSpan.append(iconImage);
         figCaptionSpan.append(activityDiv);
         figCaptionWithIcon.append(figCaptionSpan);
-        figCaptionWithIcon.className = 'figure-image-overlay';
-        anchor.href = div.querySelectorAll('p')[0].querySelector('strong').querySelector('a').href;
-        figure.append(figCaptionWithIcon);
+        figCaptionWithIcon.className = 'figure-image-overlay'; figure.append(figCaptionWithIcon);
+        div.remove();
       }
-      div.remove();
     });
     anchor.append(figure);
     li.append(anchor);
