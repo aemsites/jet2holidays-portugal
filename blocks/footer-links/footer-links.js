26f735aaddb137af9b25bb94e788b7005f16fd70
anica <anica@adobe.com>
2024-11-14 11:17:29 +0200

    apply resize recommendations

--- blocks/footer-links/footer-links.js
+++ blocks/footer-links/footer-links.js
@@ -1,66 +1,32 @@
 import {
-  domEl, h6, h5,
+  domEl, h6, h5, span,
 } from '../../scripts/dom-helpers.js';
-
-const handleScreenResize = (expandedView, parentEl) => {
-  const screenWidth = window.innerWidth;
-
-  if (screenWidth < 600 && expandedView === true) {
-    parentEl.classList.remove('open');
-    return false;
-  }
-
-  if (screenWidth > 600 && expandedView === false) {
-    parentEl.classList.add('open');
-    return true;
-  }
-
-  return null;
-};
+import { decorateIcons, fetchPlaceholders } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
+  const ph = await fetchPlaceholders();
   const parentEl = document.querySelector('.footer-links');
 
   const dom = domEl('ul');
   dom.className = 'footer-links-grid-section';
 
-  let expandedView = window.innerWidth >= 600;
-  if (expandedView) {
-    parentEl.classList.add('open');
-  }
+  const mobileHeading = domEl(
+    'div',
+    h5({ class: 'collapsible-heading' }, ph.expandFooterLinksText),
+    span({ class: 'icon icon-chevron-down' }),
+  );
 
-  // resize screen
-  window.addEventListener('resize', () => {
-    const validChange = handleScreenResize(expandedView, parentEl);
-    if (validChange !== null) {
-      expandedView = validChange;
-    }
+  mobileHeading.className = 'mobile-heading';
+  dom.append(mobileHeading);
+
+  /* click event listener logic */
+  mobileHeading.addEventListener('click', () => {
+    parentEl.classList.toggle('open');
   });
 
   [...block.children].forEach((row, index) => {
     /* mobile collapsible view */
-    if (index === 0) {
-      const headingText = row.children[0].querySelectorAll('p')[0].textContent;
-      const expandIcon = row.children[0].querySelectorAll('p')[1].querySelector('span');
-      expandIcon.className = 'expand-icon';
-
-      const mobileHeading = domEl(
-        'div',
-        h5({ class: 'collapsible-heading' }, headingText),
-        expandIcon,
-      );
-
-      mobileHeading.className = 'mobile-heading';
-      dom.append(mobileHeading);
-
-      /* click event listener logic */
-      mobileHeading.addEventListener('click', () => {
-        parentEl.classList.toggle('open');
-        expandedView = !expandedView;
-      });
-
-      return;
-    }
+    if (index === 0) return;
 
     const sectionHeading = row.children[0].querySelector('p');
     const sectionLinks = row.children[1].querySelector('ul');
@@ -77,4 +43,5 @@ export default async function decorate(block) {
   });
 
   block.replaceChildren(dom);
+  decorateIcons(block);
 }
