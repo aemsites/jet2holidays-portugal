85f5642ccf240b6c582914d06ae07d33cc022fba
Robert Necsoiu <rnecsoiu@adobe.com>
2024-11-14 10:13:44 +0200

    [issue-64] - remove icon li

--- blocks/contact-us/contact-us.js
+++ blocks/contact-us/contact-us.js
@@ -1,12 +1,3 @@
-function removeButtonFromIcon(iconElement) {
-  if (iconElement.parentElement.tagName !== 'A') {
-    return;
-  }
-  iconElement.parentElement.parentElement.replaceWith(iconElement.parentElement);
-  iconElement.parentElement.removeAttribute('class');
-}
 export default async function decorate(block) {
   [...block.children].forEach((col) => col.classList.add('flex-item'));
-  const icons = document.querySelectorAll('.icon');
-  icons.forEach((icon) => removeButtonFromIcon(icon));
 }
