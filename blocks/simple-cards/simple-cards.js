31dab2559e7b36804de08f12fda345456239b0b7
anica <anica@adobe.com>
2024-11-19 19:40:54 +0200

    added optional button in block

--- blocks/simple-cards/simple-cards.js
+++ blocks/simple-cards/simple-cards.js
@@ -4,9 +4,25 @@ import {
 
 export default function decorate(block) {
   const dom = domEl('div', { class: 'simple-cards-container' });
+  const containsButton = block.children[0].querySelectorAll('div').length === 1;
+
+  if (containsButton) {
+    const seeAllButton = domEl('div', { class: 'see-all-button' });
+    const buttonContents = block.children[0].querySelector('div').querySelector('p');
+    [...buttonContents.children].forEach((child) => {
+      seeAllButton.append(child);
+    });
+    dom.append(seeAllButton);
+  }
+
   const cardsList = domEl('ul', { class: 'cards-grid-section' });
 
-  [...block.children].forEach((row) => {
+  [...block.children].forEach((row, index) => {
+    /* Skip first row if the seeAllButton is present */
+    if (index === 0 && containsButton) {
+      return;
+    }
+
     const cardPicture = row.children[0].querySelector('picture');
     const cardCaption = row.children[1].querySelectorAll('p')[1]?.querySelector('strong a') || row.children[1].querySelector('p strong a');
     const cardIcon = block.classList.contains('icon')
