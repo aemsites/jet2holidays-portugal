356f58e7909818573678841dfc1d4d68d04007b1
anica <anica@adobe.com>
2024-11-20 13:40:10 +0200

    fixed linting issue

--- blocks/unordered-list-links/unordered-list-links.js
+++ blocks/unordered-list-links/unordered-list-links.js
@@ -1,6 +1,5 @@
 export default async function decorate(block) {
-  const unorderedList = block.children[0].querySelector('ul');
-  unorderedList.className = 'links-list';
+  const unorderedList = block.children[0].querySelector('ul', { className: 'links-list' });
 
   const listItems = unorderedList.querySelectorAll('li');
   listItems.forEach((li) => {
